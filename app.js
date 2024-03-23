c325dcfa8ebed76b3a1633d7a2a4b06dfe7f202d
Serge-boro <serhiiborodin83@gmail.com>
2024-03-22 22:43:41 -0400

    first commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const path = require('path')
 
 const app = express()
 
-app.use(express.static(path.resolve(__dirname, './comfy-store/dist')))
+app.use(express.static(path.resolve(__dirname, './public')))
 app.use(express.json())
 app.use(cors())
 
@@ -32,4 +32,3 @@ const start = async () => {
 }
 
 start()
-
