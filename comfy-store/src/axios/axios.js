14767a0c6a7ee00aa2d07a51fa9f73c12337aaf9
Serge-boro <serhiiborodin83@gmail.com>
2024-04-23 18:02:38 -0400

    first commit

--- comfy-store/src/axios/axios.js
+++ comfy-store/src/axios/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-// const BASE_URL = 'http://localhost:4002/store'
-const BASE_URL = '/'
+const BASE_URL = 'http://localhost:4002/store'
+// const BASE_URL = '/'
 
 export default axios.create({
   baseURL: BASE_URL,
