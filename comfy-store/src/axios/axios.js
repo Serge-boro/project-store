850a3df6af90f492879ae5ab2cb6b1bb8bc5b887
Serge-boro <serhiiborodin83@gmail.com>
2024-04-23 08:04:52 -0400

    first commit

--- comfy-store/src/axios/axios.js
+++ comfy-store/src/axios/axios.js
@@ -6,7 +6,6 @@ export default axios.create({
   withCredentials: true,
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
   },
 })
 
@@ -15,6 +14,5 @@ export const axiosPrivate = axios.create({
   withCredentials: true,
   headers: {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': 'true',
   },
 })
