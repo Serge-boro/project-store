7a81a14622f91eb8bb9dc258683cf5eeb24926c4
Serge-boro <serhiiborodin83@gmail.com>
2024-04-23 07:45:45 -0400

    first commit

--- comfy-store/src/axios/axios.js
+++ comfy-store/src/axios/axios.js
@@ -5,7 +5,10 @@ export default axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:5173',
+    'Access-Control-Allow-Origin': [
+      'http://localhost:5173',
+      'https://front-backend-project.netlify.app',
+    ],
   },
 })
 
@@ -13,7 +16,9 @@ export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:5173',
-    
+    'Access-Control-Allow-Origin': [
+      'http://localhost:5173',
+      'https://front-backend-project.netlify.app',
+    ],
   },
 })
